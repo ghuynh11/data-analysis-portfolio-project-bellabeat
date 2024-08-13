517a678e43d990b4afcff776b02e9806b183b0a5
Giang Huynh <111240966+ghuynh11@users.noreply.github.com>
2024-08-13 19:49:09 +0200

    Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -107,8 +107,7 @@ FROM
  ON a.Id = s.Id AND a.activitydate = s.activityday
  LEFT JOIN
  daily_sleep_custom AS sl
- ON a.Id = sl.Id
- AND a.activitydate = sl.default_date
+ ON a.Id = sl.Id AND a.activitydate = sl.default_date
 GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
 ORDER BY activitydate
 )
@@ -125,6 +124,8 @@ FROM daily_analysis
 WHERE totaltimeinbed IS NOT NULL
 GROUP BY Id
 ORDER BY Id;
+
+-- 3. Analysis on how people sleep
 
 -- Sleep related product could be a possibility -> check to see if/how people sleep during the day
 -- Assuming a nap is any time someone goes to sleep and wakes up in the same day
@@ -157,183 +158,132 @@ WHERE
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
+-- 4. Analysis on intensity level
+
 -- Analyze based on time of the day and day of the week
 -- Smooth over anomalous days
-WITH
- user_dow_summary AS (
+WITH user_dow_summary AS (
  SELECT
-   Id,
-   FORMAT_TIMESTAMP("%w", ActivityDay) AS dow_number,
-   FORMAT_TIMESTAMP("%A", ActivityDay) AS day_of_week,
-   CASE
-     WHEN FORMAT_TIMESTAMP("%A", ActivityDay) IN ("Sunday", "Saturday") THEN "Weekend"
-     WHEN FORMAT_TIMESTAMP("%A", ActivityDay) NOT IN ("Sunday",
-     "Saturday") THEN "Weekday"
-   ELSE
-   "ERROR"
- END
-   AS part_of_week,
-   CASE
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_START, 0, 0) AND TIME(MORNING_END, 0, 0) THEN "Morning"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_END,
-     0,
-     0)
-   AND TIME(AFTERNOON_END,
-     0,
-     0) THEN "Afternoon"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(AFTERNOON_END, 0, 0) AND TIME(EVENING_END, 0, 0) THEN "Evening"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) >= TIME(EVENING_END,
-     0,
-     0)
-   OR TIME(TIMESTAMP_TRUNC(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour), MINUTE)) <= TIME(MORNING_START,
-     0,
-     0) THEN "Night"
-   ELSE
-   "ERROR"
- END
-   AS time_of_day,
-   SUM(TotalIntensity) AS total_intensity,
-   SUM(AverageIntensity) AS total_average_intensity,
-   AVG(AverageIntensity) AS average_intensity,
-   MAX(AverageIntensity) AS max_intensity,
-   MIN(AverageIntensity) AS min_intensity
+  Id,
+  FORMAT_TIMESTAMP("%w", ActivityDay) AS dow_number,
+  FORMAT_TIMESTAMP("%A", ActivityDay) AS day_of_week,
+  CASE
+   WHEN FORMAT_TIMESTAMP("%A", ActivityDay) IN ("Sunday", "Saturday") THEN "Weekend"
+   WHEN FORMAT_TIMESTAMP("%A", ActivityDay) NOT IN ("Sunday", "Saturday") THEN "Weekday"
+   ELSE "ERROR"
+  END AS part_of_week,
+  CASE
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_START, 0, 0) AND TIME(MORNING_END, 0, 0) THEN "Morning"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_END, 0, 0)
+    AND TIME(AFTERNOON_END, 0, 0) THEN "Afternoon"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(AFTERNOON_END, 0, 0) AND TIME(EVENING_END, 0, 0) THEN "Evening"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) >= TIME(EVENING_END, 0, 0)
+    OR TIME(TIMESTAMP_TRUNC(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour), MINUTE)) <= TIME(MORNING_START, 0, 0) THEN "Night"
+   ELSE "ERROR"
+  END AS time_of_day,
+  SUM(TotalIntensity) AS total_intensity,
+  SUM(AverageIntensity) AS total_average_intensity,
+  AVG(AverageIntensity) AS average_intensity,
+  MAX(AverageIntensity) AS max_intensity,
+  MIN(AverageIntensity) AS min_intensity
  FROM
    `data-analysis-portfolio-407018.FitBit_Dataset.HourlyIntensities`
- GROUP BY
-   1,
-   2,
-   3,
-   4,
-   5),
+ GROUP BY 1, 2, 3, 4, 5),
  intensity_deciles AS (
  SELECT
-   DISTINCT dow_number,
-   part_of_week,
-   day_of_week,
-   time_of_day,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.1) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_first_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.2) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_second_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.3) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_third_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.4) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_fourth_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.6) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_sixth_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.7) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_seventh_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.8) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_eigth_decile,
-   ROUND(PERCENTILE_CONT(total_intensity,
-       0.9) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_ninth_decile
+  DISTINCT dow_number,
+  part_of_week,
+  day_of_week,
+  time_of_day,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.1) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_first_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.2) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_second_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.3) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_third_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.4) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_fourth_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.6) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_sixth_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.7) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_seventh_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.8) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_eigth_decile,
+  ROUND(PERCENTILE_CONT(total_intensity, 0.9) OVER (PARTITION BY dow_number, part_of_week, day_of_week, time_of_day),4) AS total_intensity_ninth_decile
  FROM
-   user_dow_summary ),
+  user_dow_summary),
  basic_summary AS (
  SELECT
-   part_of_week,
-   day_of_week,
-   time_of_day,
-   SUM(total_intensity) AS total_total_intensity,
-   AVG(total_intensity) AS average_total_intensity,
-   SUM(total_average_intensity) AS total_total_average_intensity,
-   AVG(total_average_intensity) AS average_total_average_intensity,
-   SUM(average_intensity) AS total_average_intensity,
-   AVG(average_intensity) AS average_average_intensity,
-   AVG(max_intensity) AS average_max_intensity,
-   AVG(min_intensity) AS average_min_intensity
+  part_of_week,
+  day_of_week,
+  time_of_day,
+  SUM(total_intensity) AS total_total_intensity,
+  AVG(total_intensity) AS average_total_intensity,
+  SUM(total_average_intensity) AS total_total_average_intensity,
+  AVG(total_average_intensity) AS average_total_average_intensity,
+  SUM(average_intensity) AS total_average_intensity,
+  AVG(average_intensity) AS average_average_intensity,
+  AVG(max_intensity) AS average_max_intensity,
+  AVG(min_intensity) AS average_min_intensity
  FROM
-   user_dow_summary
- GROUP BY
-   1,
-   dow_number,
-   2,
-   3)
+  user_dow_summary
+ GROUP BY 1, dow_number, 2, 3)
+
 SELECT
  *
 FROM
  basic_summary
 LEFT JOIN
  intensity_deciles
-USING
- (part_of_week,
-   day_of_week,
-   time_of_day)
-ORDER BY
- 1,
- dow_number,
- 2,
+USING (
+ part_of_week,
+ day_of_week,
+ time_of_day
+ )
+ORDER BY 1, dow_number, 2,
  CASE
-   WHEN time_of_day = "Morning" THEN 0
-   WHEN time_of_day = "Afternoon" THEN 1
-   WHEN time_of_day = "Evening" THEN 2
-   WHEN time_of_day = "Night" THEN 3
-END
- ;
+  WHEN time_of_day = "Morning" THEN 0
+  WHEN time_of_day = "Afternoon" THEN 1
+  WHEN time_of_day = "Evening" THEN 2
+  WHEN time_of_day = "Night" THEN 3
+END;
 
 -- Analyze sleep duration over weekend and weekday
-WITH
- user_dow_summary AS (
+WITH user_dow_summary AS (
  SELECT
-   *,
-   FORMAT_DATE("%w", SleepDay) AS dow_number,
-   FORMAT_DATE("%A", SleepDay) AS day_of_week,
-   CASE
-     WHEN FORMAT_DATE("%A", SleepDay) IN ("Sunday", "Saturday") THEN "Weekend"
-     WHEN FORMAT_DATE("%A", SleepDay) NOT IN ("Sunday",
-     "Saturday") THEN "Weekday"
-   ELSE
-   "ERROR"
- END
-   AS part_of_week,
-   TotalTimeInBed - TotalMinutesAsleep AS minutes_awake
+  *,
+  FORMAT_DATE("%w", SleepDay) AS dow_number,
+  FORMAT_DATE("%A", SleepDay) AS day_of_week,
+  CASE
+   WHEN FORMAT_DATE("%A", SleepDay) IN ("Sunday", "Saturday") THEN "Weekend"
+   WHEN FORMAT_DATE("%A", SleepDay) NOT IN ("Sunday", "Saturday") THEN "Weekday"
+   ELSE "ERROR"
+  END AS part_of_week,
+  TotalTimeInBed - TotalMinutesAsleep AS minutes_awake
  FROM
-   data-analysis-portfolio-407018.FitBit_Dataset.SleepDay_1
+  data-analysis-portfolio-407018.FitBit_Dataset.SleepDay_1
  )
-SELECT * FROM user_dow_summary
+SELECT * 
+FROM user_dow_summary
 
 -- Combine intensity and summary of all users
-
-WITH
- user_dow_summary AS (
+WITH user_dow_summary AS (
  SELECT
-   Id,
-   FORMAT_TIMESTAMP("%w", ActivityDay) AS dow_number,
-   FORMAT_TIMESTAMP("%A", ActivityDay) AS day_of_week,
-   CASE
-     WHEN FORMAT_TIMESTAMP("%A", ActivityDay) IN ("Sunday", "Saturday") THEN "Weekend"
-     WHEN FORMAT_TIMESTAMP("%A", ActivityDay) NOT IN ("Sunday",
-     "Saturday") THEN "Weekday"
-   ELSE
-   "ERROR"
- END
-   AS part_of_week,
-   CASE
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_START, 0, 0) AND TIME(MORNING_END, 0, 0) THEN "Morning"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_END,
-     0,
-     0)
-   AND TIME(AFTERNOON_END,
-     0,
-     0) THEN "Afternoon"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(AFTERNOON_END, 0, 0) AND TIME(EVENING_END, 0, 0) THEN "Evening"
-     WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) >= TIME(EVENING_END,
-     0,
-     0)
-   OR TIME(TIMESTAMP_TRUNC(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour), MINUTE)) <= TIME(MORNING_START,
-     0,
-     0) THEN "Night"
-   ELSE
-   "ERROR"
- END
-   AS time_of_day,
-   AverageIntensity,
-   SUM(TotalIntensity) AS total_intensity,
-   SUM(AverageIntensity) AS total_avg_intensity
+  Id,
+  FORMAT_TIMESTAMP("%w", ActivityDay) AS dow_number,
+  FORMAT_TIMESTAMP("%A", ActivityDay) AS day_of_week,
+  CASE
+   WHEN FORMAT_TIMESTAMP("%A", ActivityDay) IN ("Sunday", "Saturday") THEN "Weekend"
+   WHEN FORMAT_TIMESTAMP("%A", ActivityDay) NOT IN ("Sunday", "Saturday") THEN "Weekday"
+   ELSE "ERROR"
+  END AS part_of_week,
+  CASE
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_START, 0, 0) AND TIME(MORNING_END, 0, 0) THEN "Morning"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(MORNING_END, 0, 0) AND TIME(AFTERNOON_END, 0, 0) THEN "Afternoon"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) BETWEEN TIME(AFTERNOON_END, 0, 0) AND TIME(EVENING_END, 0, 0) THEN "Evening"
+   WHEN TIME(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour)) >= TIME(EVENING_END, 0, 0)
+    OR TIME(TIMESTAMP_TRUNC(PARSE_TIMESTAMP('%I:%M:%S %p', ActivityHour), MINUTE)) <= TIME(MORNING_START, 0, 0) THEN "Night"
+  ELSE "ERROR"
+  END AS time_of_day,
+  AverageIntensity,
+  SUM(TotalIntensity) AS total_intensity,
+  SUM(AverageIntensity) AS total_avg_intensity
  FROM
-   `data-analysis-portfolio-407018.FitBit_Dataset.HourlyIntensities`
- GROUP BY
-   1, 2, 3, 4, 5, 6
+  `data-analysis-portfolio-407018.FitBit_Dataset.HourlyIntensities`
+ GROUP BY 1, 2, 3, 4, 5, 6
  )
 
 SELECT
@@ -350,12 +300,10 @@ FROM
  user_dow_summary AS HI
  LEFT JOIN data-analysis-portfolio-407018.FitBit_Dataset.HourlyCalories AS HC
  ON HI.Id = HC.Id
-GROUP BY
- 1, 2, 3, 4, 5, 6, 7
+GROUP BY 1, 2, 3, 4, 5, 6, 7
 
-
--- Analyze based on achievement groups
-
+-- 5. Analysis based on achievement groups
+ 
 WITH daily_activity_custom AS (
  SELECT
   EXTRACT (WEEK FROM activitydate) AS week_number,
@@ -379,11 +327,11 @@ SELECT
  SUM(lightlyactiveminutes) + SUM(sedentaryminutes) AS total_weekly_notactive_m
 FROM 
  daily_activity_custom
-GROUP BY 1,2
-ORDER BY 1,2
+GROUP BY 1, 2
+ORDER BY 1, 2
 ),
 
--- finding all weeks that have active mins >= 150
+-- Extract all weeks that have active mins >= 150
 highly_active_week AS (
 SELECT 
  week_number, 
@@ -402,7 +350,7 @@ ORDER BY
  Id
 ),
 
--- creating achievement groups
+-- Create achievement groups
 achievement_grp AS (
 SELECT 
  Id,
@@ -421,7 +369,7 @@ GROUP BY Id
 ORDER BY Id
 )
 
--- percentage of weeks a participant is successful in achieving ≥ 150 active minutes
+-- Percentage of weeks a participant is successful in achieving ≥ 150 active minutes
 SELECT 
  achievement_group, 
  COUNT(*),
